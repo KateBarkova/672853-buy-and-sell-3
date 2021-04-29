166311dc3c21361c211b4472c128783d70c99a65
Barkova <barkova.kate@yandex.ru>
2021-04-29 23:49:52 +0300

    refs #module2-task2 *create server

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -3,11 +3,14 @@
 const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
+const server = require(`./server`);
+
 
 const cli = {
   [generate.name]: generate,
   [help.name]: help,
   [version.name]: version,
+  [server.name]: server,
 };
 
 module.exports = {
