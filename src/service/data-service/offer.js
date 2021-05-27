f9f00edbbf5c8c16fd94af2427e101dbbd0cb3f1
barkova <barkova.kate@yandex.ru>
2021-05-27 15:43:58 +0300

    refs #module4-task1 *create api

---
+++ src/service/data-service/offer.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const {nanoid} = require(`nanoid`);
+const {MAX_ID_LENGTH} = require(`../serviceConstants`);
+
+class OfferService {
+  constructor(offers) {
+    this._offers = offers;
+  }
+
+  create(offer) {
+    const newOffer = Object
+      .assign({id: nanoid(MAX_ID_LENGTH), comments: []}, offer);
+
+    this._offers.push(newOffer);
+    return newOffer;
+  }
+
+  find(id) {
+    return this._offers.find((item) => item.id === id);
+  }
+
+  drop(id) {
+    const offer = this.find(id);
+
+    if (!offer) {
+      return null;
+    }
+
+    this._offers = this._offers.filter((item) => item.id !== id);
+    return offer;
+  }
+
+  findAll() {
+    return this._offers;
+  }
+
+  findOne(id) {
+    return this.find(id);
+  }
+
+  update(id, offer) {
+    const oldOffer = this.find(id);
+
+    return Object.assign(oldOffer, offer);
+  }
+
+}
+
+module.exports = OfferService;
