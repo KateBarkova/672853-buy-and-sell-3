9389141bbd3256e1ddb6dc3ada3568298b7a5cbe
Barkova <barkova.kate@yandex.ru>
2021-06-06 22:36:01 +0300

    refs #module4-task3 *api for main page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,7 +3,14 @@
 const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+const {getAPI} = require(`../api`);
+const api = getAPI();
+
+mainRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main`, {offers});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
